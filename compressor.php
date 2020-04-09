ad8700172ba22a121dff4ef4ccd3d8f4fc990d42
Valentin B <34310553+Valbou@users.noreply.github.com>
2020-04-09 10:18:06 +0200

    Update compressor.php
    Change le fichier txt de base

--- compressor.php
+++ compressor.php
@@ -67,7 +67,7 @@
     $debut = microtime(TRUE);
 
     $repo = "/home/user/";
-    $texte = file_get_contents($repo."texte.txt");
+    $texte = file_get_contents($repo."dico_fr.txt");
     script($texte);
 
     $fin = microtime(TRUE);
